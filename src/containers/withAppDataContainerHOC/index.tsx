eaf90916d17becd4853af291c43b25121c386ce9
John Britto <M1052868@mindtree.com>
2021-03-22 03:28:09 +0530

    configured wss

--- src/containers/withAppDataContainerHOC/index.tsx
+++ src/containers/withAppDataContainerHOC/index.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
 import { IServices } from "../../components/pages/Overview/index.types";
 
 const apiURL = "http://13.232.99.42";
@@ -16,7 +17,7 @@ const withAppDataContainerHOC = ({ services }: any) => (WrappedComponent: any) =
             let callArray: any = []
             if (Array.isArray(services)) {
                 services.map((request: IServices) => {
-                    callArray.push(fetch(`${apiURL}${request.uri}`).then((res) => res.json()));
+                    callArray.push(fetch(`${apiURL}${request.uri}`, { mode: 'cors' }).then((res) => res.json()));
                 })
             }
 
@@ -28,7 +29,21 @@ const withAppDataContainerHOC = ({ services }: any) => (WrappedComponent: any) =
                     });
                     this.setState({data: this.result})
                 })
-            })
+            });
+
+
+            const socket = socketIOClient("wss://13.232.99.42", {path: "/js/socket.io.min.js", transports: ['polling']});
+
+            socket.on("ALL", (data: any) => this.setResponse(data));
+        }
+
+        /**Since WSS is not connecting as expected data retrieval from Web socket is still pending. */
+        setResponse(data: any) {
+            console.log(data);
+        }
+
+        handleData(data: any) {
+            let result = JSON.parse(data); console.log(result)
         }
 
         render() {
